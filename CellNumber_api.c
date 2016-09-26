695715ed3092b9e7a5125c4637da82b9f2ceb554
nickel zhang <dulu150@126.com>
2016-09-26 09:13:42 +0800

    增加解压缩功能:ADD decompress function
    增加号码解压缩功能，将压缩后的字符串解还原为原始字符。
    add number decompress function.

--- CellNumber_api.c
+++ CellNumber_api.c
@@ -30,9 +30,37 @@ int CellNumberStrCompress(char* pSrcCellNumber, char* pResult)
 	}
 
 	/* 循环遍历源字符串进行转换，转换过程中检查是否存在非法字符 */
-	for (ulIndex = 0; ulIndex < 100; ulIndex++)
+	for (ulIndex = 0; ulIndex < (ulCellNumLen / 2); ulIndex++)
 	{
 		pResult[ulIndex] = pSrcCellNumber[ulIndex*2] + pSrcCellNumber[ulIndex*2 + 1];
+	}
+
+	return 0;
+}
+
+int CellNumberStrDeCompress(char* pSrcCellNumber, char* pResult)
+{
+	int ulIndex = 0;
+	int ulCellNumLen = strlen(pSrcCellNumber);
+
+	/* 入参检查 */
+	if((NULL == pSrcCellNumber) || (NULL == pResult))
+	{
+		printf("Invalid Input:NULL ptr");
+		return -1;
+	}
+
+	if (0 >= ulCellNumLen)
+	{
+		printf("Invalid Input:Input str's length is [%d]", ulCellNumLen);
+		return -1;
+	}
+
+	/* 循环遍历源字符串进行转换，转换过程中检查是否存在非法字符 */
+	for (ulIndex = 0; ulIndex < ulCellNumLen; ulIndex++)
+	{
+		pResult[ulIndex*2] = (pSrcCellNumber[ulIndex] / 10) % 10;
+		pResult[ulIndex*2 + 1] = pSrcCellNumber[ulIndex] % 10;
 	}
 
 	return 0;
@@ -43,9 +71,11 @@ int main()
 	/* 单元测试代码 */
 	char aStrSrcCellNumber[] = "86138123456";
 	char* aCellNumberAfterCompress = NULL;
+	char* aCellNumberAfterDeCompress = NULL;
 
-	/* 为保存结果的缓冲区申请内存 */
+	/* 压缩测试：为保存结果的缓冲区申请内存 */
 	aCellNumberAfterCompress = (char *)malloc(sizeof(char) * strlen(aStrSrcCellNumber));
+	memset(aCellNumberAfterCompress, 0, strlen(aStrSrcCellNumber));
 	if (NULL == aCellNumberAfterCompress)
 	{
 		printf("malloc failed");
@@ -54,9 +84,30 @@ int main()
 	else
 	{
 		CellNumberStrCompress(aStrSrcCellNumber, aCellNumberAfterCompress);
+		printf("result = [%s]", aCellNumberAfterCompress);
 	}
+
+	/* 解压缩测试 */
+	aCellNumberAfterDeCompress = (char *)malloc(sizeof(char) * strlen(aStrSrcCellNumber));
+	memset(aCellNumberAfterDeCompress, 0, strlen(aStrSrcCellNumber));
+	if (NULL == aCellNumberAfterDeCompress)
+	{
+		printf("malloc failed");
+		return -1;
+	} 
+	else
+	{
+		CellNumberStrDeCompress(aCellNumberAfterCompress, aCellNumberAfterDeCompress);
+		printf("result = [%s]", aCellNumberAfterDeCompress);
+	}
+
+	/* 处理完成回退内存申请 */
 	free(aCellNumberAfterCompress);
 	aCellNumberAfterCompress = NULL;
+
+	free(aCellNumberAfterCompress);
+	aCellNumberAfterCompress = NULL;
+
 	getchar();
 	return 0;
 }
